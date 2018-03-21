c4ad0c31389b5b89cbdfab196410d0a84bf168f2
SUTTROOGUN Yogin Kumar <yo.suttroogun@gmail.com>
2018-03-21 23:12:38 +0400

    Last commit

--- src/main/java/com/wish/list/wishList/controller/UserController.java
+++ src/main/java/com/wish/list/wishList/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.wish.list.wishList.dto.UserDto;
 import com.wish.list.wishList.dto.UserResetPasswordDto;
 import com.wish.list.wishList.service.UserService;
